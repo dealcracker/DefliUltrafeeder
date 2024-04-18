d4ba668d417f121cd5c79cde136eeceef2bc0106
Ken Kiplinger <ken@roundhousesoftware.com>
2024-04-17 22:02:13 -0400

    Update Script

--- installUltrafeeder.sh
+++ installUltrafeeder.sh
@@ -104,8 +104,9 @@ apt-get -qq update -y
   # fi
 
 ip_address=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}') 
+
 echo
-echo "Using IP address: $ip_address"
+echo "Using local IP address: $ip_address"
 echo ""
 echo "Removing any existing dockers..."
 
